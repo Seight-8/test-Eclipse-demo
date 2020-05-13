be73789c66e3888936742aa4dd93e4a77af10cc9
Seight-8 <seight.8@mail.ru>
2020-05-13 09:23:27 +0500

    Updates

---
+++ precache-manifest.2fade670ab4f417284457cd847b13175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32261ebebb0c4f1cca190b7f9cc4f4db",
+    "url": "/test-Eclipse-demo/index.html"
+  },
+  {
+    "revision": "e1fa537edaed33b00080",
+    "url": "/test-Eclipse-demo/static/css/main.df69c64f.chunk.css"
+  },
+  {
+    "revision": "0240fe62aece8be2cb5e",
+    "url": "/test-Eclipse-demo/static/js/2.735a8de1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-Eclipse-demo/static/js/2.735a8de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1fa537edaed33b00080",
+    "url": "/test-Eclipse-demo/static/js/main.9a6cbc54.chunk.js"
+  },
+  {
+    "revision": "cdf4696479719c861c09",
+    "url": "/test-Eclipse-demo/static/js/runtime-main.1658d3d3.js"
+  }
+]);
